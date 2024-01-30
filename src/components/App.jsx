e3bb547abfe67d70243d6d648e11bf1149cd35aa
Volodymyr <61534586+VolodymyrBilichenko@users.noreply.github.com>
2024-01-30 12:42:22 +0200

    u

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,40 +1,39 @@
-import { useEffect, useState } from "react";
-import { GlobalStyle } from "./GlobalStyle/GlobalStyle.styled";
-import { ImageGallery } from "./ImageGallery/ImageGallery";
-import { SearchBar } from "./SearchBar/SearchBar";
-import { getAllImages } from "API/ImagesApi";
+import { useEffect, useState } from 'react';
+import { GlobalStyle } from './GlobalStyle/GlobalStyle.styled';
+import { ImageGallery } from './ImageGallery/ImageGallery';
+import { SearchBar } from './SearchBar/SearchBar';
+import { getAllImages } from 'API/ImagesApi';
 
 export const App = () => {
   const [images, setImages] = useState([]);
   const [searchQuery, setSearchQuery] = useState('');
   const [perPage, setPerPage] = useState(12);
 
-  const handleSearch = async () => {
+  const handleSearch = async (query) => {
     try {
-      setImages(await getAllImages(searchQuery, perPage));
+      setImages(await getAllImages(query, perPage));
     } catch (error) {
       console.error('Помилка отримання зображень:', error);
     }
-  }
+  };
 
   useEffect(() => {
     if (searchQuery.trim() !== '') {
-      handleSearch();
+      handleSearch(searchQuery);
     }
-  }, [searchQuery, perPage])
+  }, [searchQuery, perPage]);
 
   return (
     <>
       <GlobalStyle />
 
-      <SearchBar 
-        searchQuery={searchQuery} 
+      <SearchBar
+        searchQuery={searchQuery}
         setSearchQuery={setSearchQuery}
         onSearch={handleSearch}
       />
 
-      <ImageGallery imagesLi={images}/>
-
+      <ImageGallery imagesLi={images} />
     </>
   );
 };
--- src/components/SearchBar/SearchBar.jsx
+++ src/components/SearchBar/SearchBar.jsx
@@ -1,65 +1,56 @@
-import { HeaderSearchBarStye } from './SearchBarStyle.styled'
+import { HeaderSearchBarStye } from './SearchBarStyle.styled';
 import { ToastContainer, toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 
-export const SearchBar = ({searchQuery, setSearchQuery, onSearch}) => {
+export const SearchBar = ({ searchQuery, setSearchQuery, onSearch }) => {
 
-    const handleInput = (evt) => {
-        setSearchQuery(evt.currentTarget.value)
+  const handleInput = (evt) => {
+    setSearchQuery(evt.currentTarget.value);
+  };
+
+  const handleSubmit = (evt) => {
+    evt.preventDefault();
+
+    if (searchQuery.trim() === '') {
+      toast.info('Please fill in the input field');
+      return;
     }
 
-    // const handleSearch = (evt) => {
-    //     evt.preventDefault();
-    //     onSearch(searchQuery)
-    //     onFormReset();
-    // }
+    // onSearch(searchQuery);
+    setSearchQuery(''); // Очистіть поле вводу після відправки запиту
+  };
 
-    const handleSubmit = (evt) => {
-        evt.preventDefault();
+  return (
+    <div>
+      <ToastContainer
+        position='top-right'
+        autoClose={2000}
+        hideProgressBar={false}
+        newestOnTop={false}
+        closeOnClick
+        rtl={false}
+        pauseOnFocusLoss
+        draggable
+        pauseOnHover
+        theme='dark'
+      />
 
-        if (searchQuery.trim() === '') {
-            toast.info('Please fill in the input field')
-            return;
-        }
-        onSearch(searchQuery)
-        onFormReset();
-    }
+      <HeaderSearchBarStye className='searchar'>
+        <form className="form" role="search" onSubmit={handleSubmit}>
+          <button type="submit" className="button">
+            <span className="button-label">Search</span>
+          </button>
 
-    const onFormReset = () => {
-        setSearchQuery('')
-    };
-
-    return (
-        <div>
-            <ToastContainer
-            position="top-right"
-            autoClose={2000}
-            hideProgressBar={false}
-            newestOnTop={false}
-            closeOnClick
-            rtl={false}
-            pauseOnFocusLoss
-            draggable
-            pauseOnHover
-            theme="dark"
-            />
-
-            <HeaderSearchBarStye className="searchar">
-            <form className="form" role="search" onSubmit={handleSubmit}>
-                <button type="submit" className="button">
-                    <span className="button-label">Search</span>
-                </button>
-
-                <input
-                    className="input"
-                    type="search"
-                    autoComplete="off"
-                    placeholder="Search images"
-                    onChange={handleInput}
-                    value={searchQuery}
-                />
-            </form>
-            </HeaderSearchBarStye>
-        </div>
-    )
-}
+          <input
+            className="input"
+            type="search"
+            autoComplete="off"
+            placeholder="Search images"
+            onChange={handleInput}
+            value={searchQuery}
+          />
+        </form>
+      </HeaderSearchBarStye>
+    </div>
+  );
+};
